6f8b0602160e4c7c307ebe90e35da55acc480619
xiaolan <547498072@qq.com>
2018-02-11 10:25:17 +0800

    修改

--- LoadingDemo/app/src/main/java/text/com/loadingdemo/TestActivity.java
+++ LoadingDemo/app/src/main/java/text/com/loadingdemo/TestActivity.java
@@ -3,6 +3,7 @@ package text.com.loadingdemo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 /**
  * Created by Administrator on 2018/2/11 0011.
@@ -14,5 +15,7 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_activity);
+
+        Toast.makeText(this, "尝试git回覌", Toast.LENGTH_SHORT).show();
     }
 }
