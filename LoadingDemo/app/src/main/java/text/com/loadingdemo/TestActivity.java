953d2156b509d6f743753732dc72873132f932a8
xiaolan <547498072@qq.com>
2018-02-11 10:42:38 +0800

    再尝试

--- LoadingDemo/app/src/main/java/text/com/loadingdemo/TestActivity.java
+++ LoadingDemo/app/src/main/java/text/com/loadingdemo/TestActivity.java
@@ -3,6 +3,7 @@ package text.com.loadingdemo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * Created by Administrator on 2018/2/11 0011.
@@ -14,5 +15,7 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_activity);
+
+        Log.e("","再尝试");
     }
 }
