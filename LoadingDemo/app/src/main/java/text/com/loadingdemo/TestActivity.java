b6d0e100de8bfacc5ead1f785360390033ff1847
xiaolan <547498072@qq.com>
2018-02-11 10:48:24 +0800

    Revert "尝试回"
    This reverts commit e558110fce1cde9465a2f8d8511a03e185758602.
    
    # Conflicts:
    #	LoadingDemo/app/src/main/java/text/com/loadingdemo/TestActivity.java

--- LoadingDemo/app/src/main/java/text/com/loadingdemo/TestActivity.java
+++ LoadingDemo/app/src/main/java/text/com/loadingdemo/TestActivity.java
@@ -3,7 +3,10 @@ package text.com.loadingdemo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+<<<<<<< HEAD
 import android.util.Log;
+=======
+>>>>>>> parent of e558110... 尝试回
 
 /**
  * Created by Administrator on 2018/2/11 0011.
@@ -15,7 +18,10 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_activity);
+<<<<<<< HEAD
 
         Log.e("","再尝试");
+=======
+>>>>>>> parent of e558110... 尝试回
     }
 }
