7be1f557b3696a8741c12758237991aa8effd306
xiaolan <547498072@qq.com>
2018-02-11 09:58:27 +0800

    提交到本地mast

---
+++ LoadingDemo/app/src/main/java/text/com/loadingdemo/TestActivity.java
@@ -0,0 +1,18 @@
+package text.com.loadingdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2018/2/11 0011.
+ */
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_activity);
+    }
+}
